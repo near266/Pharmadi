f088e49c1c7efe693f5c8b949b53924c5ee06852
ntthang0216@gmail.com <ntthang0216@gmail.com>
2023-03-22 02:37:27 +0700

    udpate add query

---
+++ src/Jhipster.Infrastructure/Migrations/20230321171548_FixDate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jhipster.Infrastructure.Migrations
+{
+    public partial class FixDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateExp",
+                table: "WarehouseProducts");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateExpire",
+                table: "WarehouseProducts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateExpire",
+                table: "WarehouseProducts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DateExp",
+                table: "WarehouseProducts",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ src/Jhipster.Module/CatalogManagement/Module.Catalog.Application/Queries/WarehouseProductQ/CountProductQuery.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Module.Catalog.Application.Persistences;
+
+
+namespace Module.Catalog.Application.Queries.WarehouseProductQ
+{
+    public class CountProductQuery : IRequest<int>
+    {
+        public Guid id { get; set; }
+    }
+    public class CountProductQueryHandler : IRequestHandler<CountProductQuery, int>
+    {
+        private readonly IWarehouseProductRepository _repo;
+        private readonly IMapper _mapper;
+        public CountProductQueryHandler(IWarehouseProductRepository repo, IMapper mapper)
+        {
+            _repo = repo ?? throw new ArgumentNullException(nameof(repo));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+        public async Task<int> Handle(CountProductQuery request, CancellationToken cancellationToken)
+        {
+            return await _repo.CountProduct(request.id);
+        }
+    }
+
+}
