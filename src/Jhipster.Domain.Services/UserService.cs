f9636f6f81ffac6cd8a6a830615828e30bd346ab
Manh Dung <vinhdunghuy@gmail.com>
2023-03-22 02:42:33 +0700

    Fix Role Merchant

--- src/Jhipster.Domain.Services/UserService.cs
+++ src/Jhipster.Domain.Services/UserService.cs
@@ -9,6 +9,7 @@ using Jhipster.Domain.Services.Interfaces;
 using Jhipster.Dto;
 using Jhipster.Dto.Authentication;
 using Jhipster.Service.Utilities;
+using LanguageExt.Pipes;
 using LanguageExt.UnitsOfMeasure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -198,6 +199,7 @@ namespace Jhipster.Domain.Services
                 //TODO manage authorities
             };
             await _userManager.CreateAsync(newUser);
+            await CreateUserRoles(newUser, userToRegister.UserRoles.Select(iur => iur.Role.Name).ToHashSet());
             _log.LogDebug($"Created Information for User: {newUser}");
             return newUser;
         }
--- src/Jhipster/Configuration/AutoMapper/AutoMapperProfile.cs
+++ src/Jhipster/Configuration/AutoMapper/AutoMapperProfile.cs
@@ -41,7 +41,9 @@ namespace Jhipster.Configuration.AutoMapper
             CreateMap<RegisterByAdminDTO, Merchant>();
 
             CreateMap<User,RegisterRequest>()
-             .ForMember(des => des.Roles, opt => opt.MapFrom(user => user.UserRoles.Select(iur => iur.Role.Name).ToHashSet())).ReverseMap();
+             .ForMember(userDto => userDto.Roles, opt => opt.MapFrom(user => user.UserRoles.Select(iur => iur.Role.Name).ToHashSet()))
+            .ReverseMap()
+                .ForPath(user => user.UserRoles, opt => opt.MapFrom(userDto => userDto.Roles.Select(role => new UserRole { Role = new Domain.Role { Name = role }, UserId = userDto.Id }).ToHashSet()));
             CreateMap<User,RegisterAdminRequest>()
             .ForMember(userDto => userDto.Roles, opt => opt.MapFrom(user => user.UserRoles.Select(iur => iur.Role.Name).ToHashSet()))
             .ReverseMap()
