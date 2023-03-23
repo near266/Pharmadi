c0dc53adfab3447400e44e443035f9371da73944
Manh Dung <vinhdunghuy@gmail.com>
2023-03-24 01:05:14 +0700

    ViewCart

--- src/Jhipster.Module/OrderingManagement/Module.Ordering.Application/Persistences/ICartRepostitory.cs
+++ src/Jhipster.Module/OrderingManagement/Module.Ordering.Application/Persistences/ICartRepostitory.cs
@@ -10,7 +10,7 @@ namespace Module.Ordering.Application.Persistences
         Task<int> Add(Cart request);
         Task<int> Update(Cart request);
         Task<int> Delete(List<Guid> ids);
-        Task<PagedList<ViewCartByBrandDTO>> GetAllByUser(int page, int pageSize, Guid userId);
+        Task<ViewCartDTO> GetAllByUser(int page, int pageSize, Guid userId);
         Task<List<Cart>> GetCartChoice(Guid userId);
         Task<CartResultDTO> CartResultSum(Guid userId);
     }
--- src/Jhipster.Module/OrderingManagement/Module.Ordering.Application/Queries/CartQ/CartGetAllByUserQuery.cs
+++ src/Jhipster.Module/OrderingManagement/Module.Ordering.Application/Queries/CartQ/CartGetAllByUserQuery.cs
@@ -8,13 +8,13 @@ using Module.Ordering.Shared.DTOs;
 
 namespace Module.Ordering.Application.Queries.CartQ
 {
-    public class CartGetAllByUserQuery : IRequest<PagedList<ViewCartByBrandDTO>>
+    public class CartGetAllByUserQuery : IRequest<ViewCartDTO>
     {
         public int page { get; set; }
         public int pageSize { get; set; }
         public Guid userId { get; set; }
     }
-    public class CartGetAllByUserQueryHandler : IRequestHandler<CartGetAllByUserQuery, PagedList<ViewCartByBrandDTO>>
+    public class CartGetAllByUserQueryHandler : IRequestHandler<CartGetAllByUserQuery, ViewCartDTO>
     {
         private readonly ICartRepostitory _repo;
         private readonly IMapper _mapper;
@@ -23,7 +23,7 @@ namespace Module.Ordering.Application.Queries.CartQ
             _repo = repo ?? throw new ArgumentNullException(nameof(repo));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
-        public async Task<PagedList<ViewCartByBrandDTO>> Handle(CartGetAllByUserQuery request, CancellationToken cancellationToken)
+        public async Task<ViewCartDTO> Handle(CartGetAllByUserQuery request, CancellationToken cancellationToken)
         {
             return await _repo.GetAllByUser(request.page, request.pageSize, request.userId);
         }
