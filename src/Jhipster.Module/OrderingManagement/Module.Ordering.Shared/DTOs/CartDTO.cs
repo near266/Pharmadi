6d6a6b81899474ffbc895aeba0071f34b4b60c80
ntthang0216@gmail.com <ntthang0216@gmail.com>
2023-03-23 21:19:45 +0700

    update order_cart

---
+++ src/Jhipster.Module/OrderingManagement/Module.Ordering.Shared/DTOs/CartDTO.cs
@@ -0,0 +1,22 @@
+﻿using Module.Catalog.Domain.Entities;
+using Module.Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module.Ordering.Shared.DTOs
+{
+    public class CartResultDTO
+    {
+        public int Quantity { get; set; }
+        public int TotalPrice { get; set; }
+    }
+
+    public class ViewCartByBrandDTO
+    {
+        public Brand Brand { get; set; }
+        public List<Cart> Carts { get; set; }
+    }
+}
