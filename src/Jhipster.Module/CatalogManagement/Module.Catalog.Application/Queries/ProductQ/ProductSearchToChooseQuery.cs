3410f7a28bf29cbe7fc3817dffd880bb5911286a
ntthang0216@gmail.com <ntthang0216@gmail.com>
2023-03-23 11:24:38 +0700

    file lose

---
+++ src/Jhipster.Module/CatalogManagement/Module.Catalog.Application/Queries/ProductQ/ProductSearchToChooseQuery.cs
@@ -0,0 +1,30 @@
+﻿
+
+using AutoMapper;
+using Jhipster.Service.Utilities;
+using MediatR;
+using Module.Catalog.Application.Persistences;
+using Module.Catalog.Domain.Entities;
+
+namespace Module.Factor.Application.Queries.ProductQ
+{
+    public class ProductSearchToChooseQuery : IRequest<IEnumerable<Product>>
+    {
+        public string? keyword { get; set; }
+    }
+    public class ProductSearchToChooseQueryHandler : IRequestHandler<ProductSearchToChooseQuery, IEnumerable<Product>>
+    {
+        private readonly IProductRepository _repo;
+        private readonly IMapper _mapper;
+        public ProductSearchToChooseQueryHandler(IProductRepository repo, IMapper mapper)
+        {
+            _repo = repo ?? throw new ArgumentNullException(nameof(repo));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+        public async Task<IEnumerable<Product>> Handle(ProductSearchToChooseQuery request, CancellationToken cancellationToken)
+        {
+            return await _repo.SearchToChoose(request.keyword);
+        }
+    }
+
+}
