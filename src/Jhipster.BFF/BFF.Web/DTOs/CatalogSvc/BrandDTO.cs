5fec24149a534a2e8cc33e0911bd5a7ceae2620a
near266 <vuquangnam2662002@gmail.com>
2023-03-24 19:44:08 +0700

    fixbrand

---
+++ src/Jhipster.BFF/BFF.Web/DTOs/CatalogSvc/BrandDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFF.Web.DTOs.CatalogSvc
+{
+    public class AddBrandToGroupRequest
+    {
+        public List<Guid> BrandIds { get; set; }
+        public Guid GroupId { get; set; }
+    }
+}
