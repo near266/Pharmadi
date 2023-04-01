ace3628b4f2583910c58eb95100bf3d7aded6c63
Manh Dung <vinhdunghuy@gmail.com>
2023-04-01 14:19:27 +0700

    Update List Role

--- src/Jhipster/DTO/UpdateRoleDTO.cs
+++ src/Jhipster/DTO/UpdateRoleDTO.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 
 namespace Jhipster.DTO
 {
+    public class UpdateRoleDTORq
+    {
+        public List<UpdateRoleDTO> listusers { get; set; }
+    }
     public class UpdateRoleDTO
     {
         public string user { get; set; }
