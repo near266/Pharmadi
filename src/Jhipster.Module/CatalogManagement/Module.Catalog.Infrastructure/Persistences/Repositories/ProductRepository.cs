8837d1a0c1a3d1db822f1c6dc95f4ef5ecd7733c
Manh Dung <vinhdunghuy@gmail.com>
2023-04-19 10:29:45 +0700

    .

--- src/Jhipster.Module/CatalogManagement/Module.Catalog.Infrastructure/Persistences/Repositories/ProductRepository.cs
+++ src/Jhipster.Module/CatalogManagement/Module.Catalog.Infrastructure/Persistences/Repositories/ProductRepository.cs
@@ -207,7 +207,7 @@ namespace Module.Catalog.Infrastructure.Persistence.Repositories
                 Image = i.Image,
                 Specification = i.Specification,
                 SaleNumber = _context.ProductSales.Where(a => a.ProductId == i.Id).Select(a => a.Quantity).FirstOrDefault(),
-                Discount = (float?)((i.SalePrice / i.Price) * 100),
+                Discount = i.Price != 0 ? (float?)(((i.Price - i.SalePrice) / i.Price) * 100) : 0,
                 LabelProducts = _context.LabelProducts.Include(i => i.Label).Where(i => i.ProductId == i.Id).AsEnumerable(),
                 CartNumber = (userId != null) ? _context.Carts.Where(a => a.UserId == userId && a.ProductId == i.Id).Select(i => i.Quantity).FirstOrDefault().ToString() : "0",
                 CanOrder = i.CanOrder
@@ -240,7 +240,7 @@ namespace Module.Catalog.Infrastructure.Persistence.Repositories
                 Specification = i.Specification,
                 SaleNumber = _context.ProductSales.Where(a => a.ProductId == i.Id).Select(a => a.Quantity).FirstOrDefault(),
                 Archived = i.Archived,
-                Discount = i.Price != 0 ? (float?)(((i.Price - i.SalePrice) / i.Price) * 100) : 100,
+                Discount = i.Price != 0 ? (float?)(((i.Price - i.SalePrice) / i.Price) * 100) : 0,
                 LabelProducts = _context.LabelProducts.Include(i => i.Label).Where(i => i.ProductId == i.Id).AsEnumerable(),
                 CartNumber = (userId != null) ? _context.Carts.Where(a => a.UserId == userId && a.ProductId == i.Id).Select(i => i.Quantity).FirstOrDefault().ToString() : "0",
                 CanOrder = i.CanOrder
