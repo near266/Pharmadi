c797fe220ed4fbdec912e42f13dbe4613ec98fed
Manh Dung <vinhdunghuy@gmail.com>
2023-05-08 13:45:55 +0700

    product

--- src/Jhipster.Module/CatalogManagement/Module.Catalog.Application/Persistences/IProducRepository.cs
+++ src/Jhipster.Module/CatalogManagement/Module.Catalog.Application/Persistences/IProducRepository.cs
@@ -13,7 +13,7 @@ namespace Module.Catalog.Application.Persistences
         Task<Product> ViewDetail(Guid Id);
         Task<PagedList<ProductSearchDTO>> ViewProductForU( string? keyword,int page, int pageSize, Guid? userId);
         Task<PagedList<ProductSearchDTO>> ViewProductBestSale(int page, int pageSize, Guid? userId);
-        Task<PagedList<ProductSearchDTO>> ViewProductNew(int page, int pageSize, Guid? userId);
+        Task<PagedList<NewProductDTO>> ViewProductNew(int page, int pageSize, Guid? userId);
         Task<PagedList<ViewProductPromotionDTO>> ViewProductPromotion(string? keyword,int page, int pageSize, Guid? userId);
         Task<PagedList<ProductSearchDTO>> SearchProduct(string? keyword, List<Guid> categoryIds,List<Guid> cateLevel2Ids, List<Guid?>? brandIds, List<Guid?>? tagIds, int page, int pageSize,Guid? userId);
         Task<int> UpdataStatusProduct(Guid id ,int status);
