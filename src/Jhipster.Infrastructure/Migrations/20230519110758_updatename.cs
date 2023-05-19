15157a35c71466946fd6280b572b6819590dd769
near266 <vuquangnam2662002@gmail.com>
2023-05-19 22:31:51 +0700

    update clname

---
+++ src/Jhipster.Infrastructure/Migrations/20230519110758_updatename.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jhipster.Infrastructure.Migrations
+{
+    public partial class updatename : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Function",
+                table: "Products",
+                newName: "UserObject");
+
+            migrationBuilder.RenameColumn(
+                name: "Effect",
+                table: "Products",
+                newName: "Warning");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Warning",
+                table: "Products",
+                newName: "Effect");
+
+            migrationBuilder.RenameColumn(
+                name: "UserObject",
+                table: "Products",
+                newName: "Function");
+        }
+    }
+}
