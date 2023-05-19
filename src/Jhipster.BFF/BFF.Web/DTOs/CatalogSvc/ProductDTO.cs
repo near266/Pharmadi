65ee3e95ea108203396aa47d6a067450210b720e
near266 <vuquangnam2662002@gmail.com>
2023-05-19 22:44:10 +0700

    addfix

--- src/Jhipster.BFF/BFF.Web/DTOs/CatalogSvc/ProductDTO.cs
+++ src/Jhipster.BFF/BFF.Web/DTOs/CatalogSvc/ProductDTO.cs
@@ -117,7 +117,7 @@ namespace BFF.Web.DTOs.CatalogSvc
         public Guid Id { get; set; }
         public string? SKU { get; set; }
         public string? ProductName { get; set; }
-        public string? Function { get; set; }
+        public string? UserObject { get; set; }
         public decimal? Price { get; set; }
         public decimal? SalePrice { get; set; }
         public string? Description { get; set; }
@@ -128,7 +128,7 @@ namespace BFF.Web.DTOs.CatalogSvc
         public bool? HideProduct { get; set; }
         public List<string>? Image { get; set; }
         public string? Industry { get; set; }
-        public string? Effect { get; set; }
+        public string? Warning { get; set; }
         public string? Preserve { get; set; }
         public bool? Archived { get; set; }
         public string? Dosage { get; set; }
